fc75147b7bf04f4a996d0611692e0e6326a290ed
Antra DzS <dzerveaa@gmail.com>
2019-09-01 14:25:39 +0300

    Added JQuery switch for product input

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $("#dvd-disc").show();
+    $("#book, #furniture").hide();
+
+    $("#opt-select").change(function() {
+        console.log($(this).val());
+        switch ($(this).val()){
+            case "opt-book":
+                $("#book").show();
+                $("#dvd-disc, #furniture").hide();
+                break;
+            case "opt-furniture":
+                $("#furniture").show();
+                $("#dvd-disc, #book").hide();
+                break;
+            default:
+                $("#dvd-disc").show();
+                $("#book, #furniture").hide();
+        }    
+    });
+
+});
